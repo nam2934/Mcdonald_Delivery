22cbf2f5f9b915d81faaba0dc202b203767e7067
nam2934 <nam2934@naver.com>
2019-04-30 20:17:44 +0900

    make likelion folder

---
+++ Desktop/git/LikeLion/new_mtv/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length = 50)
+    text = models.TextField(default="text")
+    def __str__(self):
+        return self.name
